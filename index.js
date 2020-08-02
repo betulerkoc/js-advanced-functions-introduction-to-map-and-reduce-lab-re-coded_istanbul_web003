212d012784cdbfdfbd485abf8dd6504440ed458d
betul <bet.er@hotmail.com>
2020-08-02 10:34:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(arr) {
+    return arr.map(x => x * -1);
+}
+
+function mapToNoChange(arr) {
+    return arr;
+}
+
+function mapToDouble(arr) {
+    return arr.map(x => x * 2);
+}
+
+
+function mapToSquare(arr) {
+    return arr.map(x => x ** 2);
+}
+
+const reducer = function (accumulator, item) {
+    return accumulator + item;
+};
+
+function reduceToTotal(arr, start) {
+    if(!start) start = 0;
+    return arr.reduce(reducer, start);
+}
+
+const reducerTF = function (accumulator, item) {
+    return accumulator && item;
+};
+
+function reduceToAllTrue(arr) {
+    return arr.reduce(reducerTF) ? true : false;
+}
+
+const reducerAnyTrue = function (accumulator, item) {
+    return accumulator || item;
+};
+
+function reduceToAnyTrue(arr) {
+    return arr.reduce(reducerAnyTrue) ? true : false;
+}
